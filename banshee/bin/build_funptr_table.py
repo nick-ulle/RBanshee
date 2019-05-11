89b83bc654ad426914bfb2ba4c1571ea869826bb
Nick Ulle <nick.ulle@gmail.com>
2019-05-10 21:01:59 -0700

    Fix tabs in Python script.

--- banshee/bin/build_funptr_table.py
+++ banshee/bin/build_funptr_table.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 import sys
 import os
 import string
 
 def used_fn(name):
     used_fns = ["_hash_table_insert","hash_table_insert", "_make_hash_table",
-		"make_hash_table","seed_fn_ptr_table","_seed_fn_ptr_table",
-	        "fail","_fail","__fail","calloc","malloc"]
+        "make_hash_table","seed_fn_ptr_table","_seed_fn_ptr_table",
+            "fail","_fail","__fail","calloc","malloc"]
     return (name in used_fns)
 
 def get_table(libraries):
@@ -14,14 +14,14 @@ def get_table(libraries):
     nm = "nm"
     nmargs = ""
     for lib in libraries:
-	output = os.popen("%s %s %s" % (nm, nmargs, lib) )
-	for line in output.readlines():
-	    entry = string.split(line);
-	    if len(entry) == 3 and ("T" in entry[1]) and not "." in entry[2] and not used_fn(entry[2]):
-		if (entry[2][0] == "_"):
-		    result.append(entry[2][1:])
-		else:
-		    result.append(entry[2])
+        output = os.popen("%s %s %s" % (nm, nmargs, lib) )
+    for line in output.readlines():
+        entry = string.split(line);
+        if len(entry) == 3 and ("T" in entry[1]) and not "." in entry[2] and not used_fn(entry[2]):
+            if (entry[2][0] == "_"):
+                result.append(entry[2][1:])
+            else:
+                result.append(entry[2])
     result.sort()
     return result
 
@@ -40,12 +40,12 @@ def print_postamble():
 
 def print_protos(table):
     for entry in table:
-	print ("void %s(void);" % entry)
+        print ("void %s(void);" % entry)
 
 def print_array(table):
     print "fn_ptr fn_ptr_array[%d] = {" % len(table)
     for entry in table:
-	print "%s," % entry
+        print "%s," % entry
     print "};"
 
 def print_hash(table):
@@ -53,8 +53,8 @@ def print_hash(table):
     print "fn_ptr_table= make_hash_table(r, %d, ptr_hash, ptr_eq);" % len(table)
     count = 0
     for entry in table:
-	print "hash_table_insert(fn_ptr_table,%s,(void *)%d);" % (entry, count)
-	count = count + 1
+        print "hash_table_insert(fn_ptr_table,%s,(void *)%d);" % (entry, count)
+        count = count + 1
     print "}\n"
     print "const int num_fn_ptrs= %d;" % count
 
@@ -69,7 +69,7 @@ def print_usage_and_exit():
 
 def print_c_file():
     if (len(sys.argv) <= 1):
-	print_usage_and_exit()
+        print_usage_and_exit()
     table = get_table(sys.argv[1:])
     print_preamble()
     print_protos(table)
